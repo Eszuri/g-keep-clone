feb96b31ccdfd8975111c891305c61b7b84dca88
Eszuri <ualdo296@gmail.com>
2024-06-02 17:23:09 +0700

    update cors url

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -6,7 +6,7 @@ import 'dotenv/config'
 
 const middleware = (app) => {
     app.use(cors({
-        origin: process.env.CORS_URL,
+        origin: ['https://penyimpan-catatan.vercel.app/', 'https://penyimpan-catatan.vercel.app'],
         credentials: true,
     }));
     app.use(express.json({ limit: "10mb" }));
