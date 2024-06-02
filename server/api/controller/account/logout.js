58d30a2239d980b9a069906fdef6afde7fb5b689
Eszuri <ualdo296@gmail.com>
2024-06-02 21:04:41 +0700

    upodate cooie opt

--- server/api/controller/account/logout.js
+++ server/api/controller/account/logout.js
@@ -1,7 +1,7 @@
 import { jwtCookiesName } from "../../../config/variabelGlobal.js";
 
 const logout = (req, res) => {
-    res.clearCookie(jwtCookiesName.authToken, { httpOnly: false, secure: true, sameSite: "None", path: "/" }).send({ logout: true, message: "Logout Succes" })
+    res.clearCookie(jwtCookiesName.authToken, { httpOnly: false, secure: true, sameSite: "Strict", path: "/" }).send({ logout: true, message: "Logout Succes" })
 }
 
 export default logout;
